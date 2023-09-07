3690bf4c5b9868bb2825c9f42753292d4a879fba
ridgeowen <144303651+ridgeowen@users.noreply.github.com>
2023-09-07 07:43:43 -0600

    Initial commit

---
+++ pex02.c
@@ -0,0 +1,17 @@
+/** pex02.c
+ * =========================================================== 
+ * Name: your name
+ * Section: your section
+ * Project: assignment information 
+ * Purpose: high level description of purpose of the program
+ *          could be multiple lines
+ * Documentation: enter here
+ * =========================================================== */
+
+#include <stdio.h>
+
+int main(){
+
+  printf("thanks for playing Pig!\n");
+
+}
---
+++ pex02funcs.c
@@ -0,0 +1,20 @@
+/** pex02funcs.c
+ * =========================================================== 
+ * Name: your name
+ * Section: your section
+ * Project: assignment information 
+ * Purpose: high level description of purpose of the program
+ *          could be multiple lines
+ * =========================================================== */
+
+char getCharSafe();
+
+int rollDie();
+
+int takeTurn(char* playerName, int playerScore);
+
+void displayGameState(char* player1Name, int player1Score, char* player2Name, int player2Score);
+
+int winningPlayer(int player1Score, int player2Score);
+
+void displayWinner(char* player1Name, int player1Score, char* player2Name, int player2Score);
